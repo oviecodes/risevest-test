70cb727c9b919cb1df341a3461edcc10c968c1fd
Alexander Godwin <alecgee73@gmail.com>
2023-08-22 11:17:22 +0100

    routes working

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,28 @@
-import express from "express"
-const router = express.Router()
+import express, { Router, Response, Request, NextFunction } from "express"
+import * as createError from "http-errors"
+import user from "./user.js"
 
-export default router
+const routes = express.Router()
+
+routes.use("/user", user)
+
+routes.all("/", (req, res) => {
+  res.status(200).json({
+    status: true,
+    message: "Risevest Backend Test API v1.0",
+  })
+})
+
+routes.use(async (req, res, next) => {
+  next(createError.NotFound("Route not Found"))
+})
+
+routes.use((err, req: Request, res: Response, next: NextFunction) => {
+  console.log(err)
+  res.status(err.status || 500).json({
+    status: false,
+    message: err.message,
+  })
+})
+
+export default routes
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,23 +6,4 @@ const routes: Router = express.Router()
 
 routes.post("/register", UserController.register)
 
-routes.all("/", (req, res) => {
-  res.status(200).json({
-    status: true,
-    message: "DROPBET API v1.0",
-  })
-})
-
-routes.use(async (req, res, next) => {
-  next(createError.NotFound("Route not Found"))
-})
-
-routes.use((err, req: Request, res: Response, next: NextFunction) => {
-  console.log(err)
-  res.status(err.status || 500).json({
-    status: false,
-    message: err.message,
-  })
-})
-
 export default routes
